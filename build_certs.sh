8c63f5bfaeff72d0d5afab839087b2ca75fc1f9d
Adam Magaluk <adam@adamm-debian.illuminatingconcepts.local>
2012-08-07 18:06:06 -0400

    Initial import

---
+++ build_certs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+openssl genrsa -des3 -out server.key 1024
+
+#//Generate Certificate signing request
+openssl req -new -key server.key -out server.csr
+
+#Sign certificate with private key
+openssl x509 -req -days 3650 -in server.csr -signkey server.key -out server.crt
+
+#Remove password requirement (needed for example)
+cp server.key server.key.secure
+openssl rsa -in server.key.secure -out server.key
+
+#//Generate dhparam file
+openssl dhparam -out dh512.pem 512
